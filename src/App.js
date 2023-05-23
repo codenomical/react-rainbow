f2898e94aaedd752e9f45cc91576f5b9e985f4af
Richard Quizon <codenomics@proton.me>
2023-05-22 19:09:30 -0700

    removed boilerplate code from App.js

--- src/App.js
+++ src/App.js
@@ -31,21 +31,6 @@ function App() {
         {colorMap}
 
         <ColorForm addColor={addColor} />
-
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
